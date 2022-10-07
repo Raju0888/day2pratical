905610d7b37ffe0f4c198aa0082f5a76e12f2b19
Raju0888 <112844343+Raju0888@users.noreply.github.com>
2022-10-07 09:36:02 +0530

    Add files via upload

---
+++ frist n perfcet numbers.py
@@ -0,0 +1,12 @@
+n=int(input("enter the value of n:"))
+count=0
+a=1;
+while count<=n:
+    sum=0
+    for  i in range (1,(a/2)+1):
+        ifa%b==0
+        sum=sum+b
+        if sum==a:
+            print(a,end=' ')
+            count+=1
+            a+=1
